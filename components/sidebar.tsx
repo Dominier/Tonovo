4ce30a27ef813983d73999fc996d3364863cee97
Dominier <118577440+Dominier@users.noreply.github.com>
2023-07-28 01:03:53 -0700

    Dashboard Implementation

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -82,7 +82,7 @@ const Sidebar = () => {
                         <Link
                             href={route.href}
                             key={route.href}
-                            // hover effects
+                            // hover effects & active hover
                             className={cn(
                                 "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
                                 pathname === route.href ? "text-white bg-white/10" : "text-zinc-400",
