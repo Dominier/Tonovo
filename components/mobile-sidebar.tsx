ee6774caf33ba5fc6babbcf3cd825560f91ef6d6
Dominier <118577440+Dominier@users.noreply.github.com>
2023-07-28 00:23:58 -0700

    Hydration Error Fix

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -6,8 +6,21 @@ import { Menu } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Sidebar from "./sidebar";
+import { useEffect, useState } from "react";
+
 
 const MobileSidebar = () => {
+    /* SIMPLE FIX FOR HYDRATION ERRORS LINE 14-22 */
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
+
     return ( 
         <Sheet>
             <SheetTrigger>
