6f36ec7cfa9451f4b04f9834abed7a19a75e24d2
Dominier <118577440+Dominier@users.noreply.github.com>
2023-08-01 01:21:38 -0700

    Implemented Replicate API

---
+++ app/(dashboard)/(routes)/music/page.tsx
@@ -0,0 +1,112 @@
+"use client"
+
+import axios from "axios";
+import * as z from "zod";
+import { Music } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"; // package from npm
+import { useRouter } from "next/navigation";
+import { ChatCompletionRequestMessage } from "openai";
+import { useState } from "react";
+
+import { Heading } from "@/components/Heading";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Empty } from "@/components/empty";
+import { Loader } from "@/components/loader";
+
+import { formSchema } from "./constants";
+
+const MusicPage = () => {
+    const router = useRouter();
+    const [music, setMusic] = useState<string>(); // Connects Messages to OpenAI
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            prompt: ""
+        }
+    });
+
+    const isLoading = form.formState.isSubmitting;
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            setMusic(undefined); // it is undefined everytime we click submit
+            
+            // calls to OpenAI
+            const response = await axios.post("/api/music", values)
+
+            setMusic(response.data.audio);
+            form.reset();
+        } catch (error: any) {
+            // TODO: Open Pro Modal
+            console.log(error);
+        } finally {
+            router.refresh();
+        }
+    }
+
+    return ( 
+        <div>
+            <Heading 
+                title="Music Generation"
+                description="Turn your prompt into music."
+                icon={Music}
+                iconColor="text-emerald-500"
+                bgColor="bg-emerald-500/10"
+            />
+            <div className="px-4 lg:px-8">
+                <div>
+                    <Form {...form}>
+                        <form
+                            onSubmit={form.handleSubmit(onSubmit)}
+                            className="rounded-lg border w-full p-4 px-3 md:px-6 
+                            focus-within:shadow-sm grid grid-cols-12 gap-2"
+                        >
+                            <FormField 
+                                name="prompt"
+                                render={({ field }) => (
+                                    <FormItem className="col-span-12 lg:col-span-10">
+                                        <FormControl className="m-0 p-0">
+                                            <Input 
+                                                className="border-0 outline-none focus-visible:ring-0
+                                                focus-visible:ring-transparent"
+                                                disabled={isLoading} // disabled when form is loading
+                                                placeholder="Piano solo."
+                                                {...field} // contains onChange, onBlur, value, etc. (could be risky)
+                                            />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+                            <Button 
+                                className="col-span-12 lg:col-span-2 w-full"
+                                disabled={isLoading}
+                            >
+                                Generate
+                            </Button>
+                        </form>
+                    </Form>
+                </div>
+                <div className="space-y-4 mt-4">
+                    {isLoading && (
+                        <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+                            <Loader />
+                        </div>
+                    )}
+                    {!music && !isLoading && (
+                        <Empty label="No music generated." />
+                    )}
+                    {/* Displays messages */}
+                    <div>
+                        Music will be generated here
+                    </div>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default MusicPage;
