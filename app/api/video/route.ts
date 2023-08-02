6b90f5a2b6c2433f52fb6989cf8719ee83740e7d
Dominier <118577440+Dominier@users.noreply.github.com>
2023-08-02 01:48:58 -0700

    Implemented Replicate API for video generation

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,4 +1,4 @@
-// Connection to Replicate API for Music
+// Connection to Replicate API for Video
 
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
@@ -24,18 +24,18 @@ export async function POST(
             return new NextResponse("Prompt is required", { status: 400 });
         }
 
-        const response = await replicate.run(
-            "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
+        const response = await replicate.run( // copied from zero documentation
+            "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
             {
               input: {
-                prompt_a: prompt
+                prompt: prompt
               }
             }
           );
 
         return NextResponse.json(response);
     } catch (error) {
-        console.log("[MUSIC_ERROR]", error)
+        console.log("[VIDEO_ERROR]", error)
         return new NextResponse("Internal Error", { status: 500 });
     }
 }
