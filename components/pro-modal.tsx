ae551983e690f74c474008137fcacb175f0693f8
Dominier <118577440+Dominier@users.noreply.github.com>
2023-08-13 23:43:56 -0700

    Implemented Badge + UI/UX Badge

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,19 +1,28 @@
 "use client"
 
+import { useProModal } from "@/hooks/use-pro-modal";
 import { 
     Dialog, 
     DialogContent,
     DialogHeader,
     DialogTitle
  } from "./ui/dialog"
+import { Badge } from "./ui/badge";
 
 export const ProModal = () => {
+    const proModal = useProModal();
+
     return (
-        <Dialog open>
+        <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
             <DialogContent>
                 <DialogHeader>
                     <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
-                        Upgrade to Tonovo
+                        <div className="flex items-center gap-x-2 font-bold py-1">
+                            Upgrade to Tonovo
+                            <Badge className="uppcase text-sm py-1" variant="premium">
+                                PRO
+                            </Badge>
+                        </div>
                     </DialogTitle>
                 </DialogHeader>
             </DialogContent>
