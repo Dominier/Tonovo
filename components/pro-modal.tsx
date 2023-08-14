1e520c7ba964a2defdca8a15f09e09c557425e5f
Dominier <118577440+Dominier@users.noreply.github.com>
2023-08-13 23:14:32 -0700

    Added Modal, Modal-Provider, and Pro-modal

---
+++ components/pro-modal.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { 
+    Dialog, 
+    DialogContent,
+    DialogHeader,
+    DialogTitle
+ } from "./ui/dialog"
+
+export const ProModal = () => {
+    return (
+        <Dialog open>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
+                        Upgrade to Tonovo
+                    </DialogTitle>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+    )
+}
