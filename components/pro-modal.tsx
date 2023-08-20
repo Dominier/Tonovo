38ba854162cd5149407c0109928190e1b32522d7
Dominier <118577440+Dominier@users.noreply.github.com>
2023-08-19 23:17:18 -0700

    Implemented Stripe Checkout on Upgrade

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import { Check, Code, ImageIcon, MessageSquare, Music, VideoIcon, Zap } from "lucide-react";
+import axios from "axios";
+import { useState } from "react";
 
 import { useProModal } from "@/hooks/use-pro-modal";
 import { 
@@ -52,6 +54,20 @@ const tools = [
 
 export const ProModal = () => {
     const proModal = useProModal();
+    const [loading, setLoading] = useState(false);
+
+    const onSubscribe = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.get("/api/stripe");  // directed to the Stripe API route.ts
+            
+            window.location.href = response.data.url;
+        } catch (error) {
+            console.log(error, "STRIPE_CLIENT_ERROR");
+        } finally {
+            setLoading(false);
+        }
+    }
 
     return (
         <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
@@ -86,6 +102,7 @@ export const ProModal = () => {
                 </DialogHeader>
                 <DialogFooter>
                     <Button
+                        onClick={onSubscribe}
                         size="lg"
                         variant="premium"
                         className="w-full"
