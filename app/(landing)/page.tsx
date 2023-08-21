1766f86753ffe565cba71b2a4bcfaad963497575
Dominier <118577440+Dominier@users.noreply.github.com>
2023-08-20 22:37:45 -0700

    Created landing page nav bar

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,22 +1,10 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { LandingNavbar } from "@/components/landing-navbar";
 
 const LandingPage = () => {
     return (
-        <div>
-            Landing Page (Unprotected)
-            <div>
-                <Link href="/sign-in">
-                    <Button>
-                        Login
-                    </Button>
-                </Link>
-                <Link href="/sign-up">
-                    <Button>
-                        Register
-                    </Button>
-                </Link>
-            </div>
+        <div className="h-full">
+            <LandingNavbar />
+            
         </div>
     );
 }
